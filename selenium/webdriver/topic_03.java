effe95f3b2a666095940822cd6af374493b1e014
Duong Nguyen <duongwww.study@gmail.com>
2024-08-03 21:54:21 +0700

    finish topic_03

--- selenium/webdriver/topic_03.java
+++ selenium/webdriver/topic_03.java
@@ -1,4 +1,19 @@
 package webdriver;
 
+import org.testng.annotations.Test;
+
 public class topic_03 {
+    @Test
+    public void TC_01(){
+
+    }
+    @Test
+    public void TC_02(){
+
+    }
+    @Test
+    public void TC_03(){
+
+    }
+
 }
