9d058ce9d03e25ef5a9ce16cca6feb53019db679
Duong Nguyen <duongwww.study@gmail.com>
2024-12-02 09:53:41 +0700

    Web Browsers Exercise 1

--- selenium/webdriver/Topic_11_WebBrowser_Commands_2.java
+++ selenium/webdriver/Topic_11_WebBrowser_Commands_2.java
@@ -58,100 +58,100 @@ public class Topic_11_WebBrowser_Commands_2 {
         // goi bien homePageUrl ra
         // driver.get(homePageUrl);
 
-        // Lay ra title o page hien tai
-        driver.getTitle();
-        System.out.println("Page Title = " + driver.getTitle());
-
-        // Lay ra Window ID o page hien tai
-        driver.getWindowHandle();
-        System.out.println("Windows ID = " + driver.getWindowHandle());
-
-        // Lay ra tat ca Window ID cua cac tab/ window
-        driver.getWindowHandles();
-
-        // Lay ra source code cua page hien tai
-        driver.getPageSource();
-        System.out.println("Page Source Code= " + driver.getPageSource());
-
-        WebDriver.TargetLocator switchTo = driver.switchTo();
-
-        // alert - frame/ iframe - window/ tab
-
-        // Alert - tuong tac voi alerts
-        driver.switchTo().alert();
-
-        // frame/ iframe
-        // switch vao frame/ iframe
-        driver.switchTo().frame("");
-
-        // switch tu frame child ra parent page (1 frame)
-        driver.switchTo().defaultContent();
-
-        // switch tu frame child ra frame parent (nhieu frame long nhau)
-        driver.switchTo().parentFrame();
-
-        // Window
-        driver.switchTo().window("");
-        driver.switchTo().newWindow(WindowType.TAB).get("https://live.techpanda.org/"); //Selenium version 4
-        driver.switchTo().newWindow(WindowType.WINDOW).get("https://live.techpanda.org/"); // Selenium version 4
-
-        // set timeout de tim element (ap dung cho 2 ham findElement & findElements)
-        // truong hop khong tim thay --> cho het thoi gian --> show loi
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.manage().timeouts().implicitlyWait(Duration.ofNanos(30));
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(30));
-
-        // get duration da timeout
-        driver.manage().timeouts().getImplicitWaitTimeout();
-
-        // set timeout de cho cho page load xong (driver.get da xu li roi, khong can thiet)
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
-        // set time out de cho code JS duoc thuc thi thanh cong
-        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
-
-        // Cookies
-        driver.manage().getCookies();
-        //driver.manage().addCookie();
-
-        // browser: full screen, maximize, minimize
-        driver.manage().window().maximize();
-        driver.manage().window().minimize();
-        driver.manage().window().fullscreen();
-
-        // Set browser theo cac kich thuoc khac nhau (test responsive)
-        driver.manage().window().setSize(new Dimension(1920,1080));
-        driver.manage().window().setSize(new Dimension(1366,768));
-
-        driver.manage().window().getSize();
-
-        // set browser tai vi tri nao tren man hinh
-        driver.manage().window().setPosition(new Point(0,0));
-        driver.manage().window().getPosition();
-
-        // selenium log: browser/ driver/ network
-        driver.manage().logs().get(LogType.BROWSER);
-        driver.manage().logs().get(LogType.PERFORMANCE);
-        driver.manage().logs().get(LogType.CLIENT);
-        driver.manage().logs().get(LogType.SERVER);
-
-        driver.manage().logs().getAvailableLogTypes();
-
-        // quay lai trang truoc do
-        driver.navigate().back();
-        // chuyen tiep den trang truoc do
-        driver.navigate().forward();
-        // load lai trang hien tai
-        driver.navigate().refresh();
-
-        // mo url khong doi page load, khac voi driver.get
-        driver.navigate().to("https://live.techpanda.org/");
-        driver.navigate().to(new URL("https://live.techpanda.org/"));
-
-
-
-
-        // step log = log4j
+//        // Lay ra title o page hien tai
+//        driver.getTitle();
+//        System.out.println("Page Title = " + driver.getTitle());
+//
+//        // Lay ra Window ID o page hien tai
+//        driver.getWindowHandle();
+//        System.out.println("Windows ID = " + driver.getWindowHandle());
+//
+//        // Lay ra tat ca Window ID cua cac tab/ window
+//        driver.getWindowHandles();
+//
+//        // Lay ra source code cua page hien tai
+//        driver.getPageSource();
+//        System.out.println("Page Source Code= " + driver.getPageSource());
+//
+//        WebDriver.TargetLocator switchTo = driver.switchTo();
+//
+//        // alert - frame/ iframe - window/ tab
+//
+//        // Alert - tuong tac voi alerts
+//        driver.switchTo().alert();
+//
+//        // frame/ iframe
+//        // switch vao frame/ iframe
+//        driver.switchTo().frame("");
+//
+//        // switch tu frame child ra parent page (1 frame)
+//        driver.switchTo().defaultContent();
+//
+//        // switch tu frame child ra frame parent (nhieu frame long nhau)
+//        driver.switchTo().parentFrame();
+//
+//        // Window
+//        driver.switchTo().window("");
+//        driver.switchTo().newWindow(WindowType.TAB).get("https://live.techpanda.org/"); //Selenium version 4
+//        driver.switchTo().newWindow(WindowType.WINDOW).get("https://live.techpanda.org/"); // Selenium version 4
+//
+//        // set timeout de tim element (ap dung cho 2 ham findElement & findElements)
+//        // truong hop khong tim thay --> cho het thoi gian --> show loi
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofNanos(30));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(30));
+//
+//        // get duration da timeout
+//        driver.manage().timeouts().getImplicitWaitTimeout();
+//
+//        // set timeout de cho cho page load xong (driver.get da xu li roi, khong can thiet)
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+//
+//        // set time out de cho code JS duoc thuc thi thanh cong
+//        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
+//
+//        // Cookies
+//        driver.manage().getCookies();
+//        //driver.manage().addCookie();
+//
+//        // browser: full screen, maximize, minimize
+//        driver.manage().window().maximize();
+//        driver.manage().window().minimize();
+//        driver.manage().window().fullscreen();
+//
+//        // Set browser theo cac kich thuoc khac nhau (test responsive)
+//        driver.manage().window().setSize(new Dimension(1920,1080));
+//        driver.manage().window().setSize(new Dimension(1366,768));
+//
+//        driver.manage().window().getSize();
+//
+//        // set browser tai vi tri nao tren man hinh
+//        driver.manage().window().setPosition(new Point(0,0));
+//        driver.manage().window().getPosition();
+//
+//        // selenium log: browser/ driver/ network
+//        driver.manage().logs().get(LogType.BROWSER);
+//        driver.manage().logs().get(LogType.PERFORMANCE);
+//        driver.manage().logs().get(LogType.CLIENT);
+//        driver.manage().logs().get(LogType.SERVER);
+//
+//        driver.manage().logs().getAvailableLogTypes();
+//
+//        // quay lai trang truoc do
+//        driver.navigate().back();
+//        // chuyen tiep den trang truoc do
+//        driver.navigate().forward();
+//        // load lai trang hien tai
+//        driver.navigate().refresh();
+//
+//        // mo url khong doi page load, khac voi driver.get
+//        driver.navigate().to("https://live.techpanda.org/");
+//        driver.navigate().to("https://live.techpanda.org/");
+//
+//
+//
+//
+//        // step log = log4j
     }
 
     @Test
---
+++ selenium/webdriver/Topic_13_BrowserExercise.java
@@ -0,0 +1,111 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_13_BrowserExercise {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void TC_01_Verify_URL() {
+        driver.get("https://live.techpanda.org/");
+
+        // Click vao My Account tai footer
+        driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
+
+        // Cach 1 - Lay URL page hien tai --> luu vao 1 bien --> verify voi assert (dung bien 2 lan tro len)
+//        String loginPageUrl = driver.getCurrentUrl();
+//        Assert.assertEquals(loginPageUrl,"https://live.techpanda.org/index.php/customer/account/login/");
+
+        // Cach 2 - Lay URL page hien tai va check truc tiep (dung 1 lan)
+        Assert.assertEquals(driver.getCurrentUrl(),"https://live.techpanda.org/index.php/customer/account/login/");
+
+        // click chuyen qua trang register
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://live.techpanda.org/index.php/customer/account/create/");
+
+    }
+
+    public void TC_02_Verify_Page_Title() {
+
+        driver.get("https://live.techpanda.org/");
+
+        // Click vao My Account tai footer
+        driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
+
+        // Cach 2 - Lay Title page hien tai va check truc tiep (dung 1 lan)
+        Assert.assertEquals(driver.getTitle(),"Customer Login");
+
+        // click chuyen qua trang register
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+        Assert.assertEquals(driver.getTitle(),"Create New Customer Account");
+
+    }
+
+    public void TC_03_Navigation() {
+        driver.get("https://live.techpanda.org/");
+
+        // Click vao My Account tai footer
+        driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
+
+        // click chuyen qua trang register
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://live.techpanda.org/index.php/customer/account/create/");
+
+        // Quay lai trang truoc do
+        driver.navigate().back();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://live.techpanda.org/index.php/customer/account/login/");
+
+        // Chuyen tiep toi trang reg
+        driver.navigate().forward();
+
+        Assert.assertEquals(driver.getTitle(),"Create New Customer Account");
+
+    }
+
+    public void TC_04_Page_Source() {
+        driver.get("https://live.techpanda.org/");
+
+        // Click vao My Account tai footer
+        driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
+
+        // Tuyet doi --> equal = bang nhau
+        Assert.assertEquals(driver.getTitle(),"Customer Login");
+
+        // Tuong doi --> assert True/ False
+        Assert.assertTrue(driver.getPageSource().contains("Login or Create an Account"));
+
+        // click chuyen qua trang register
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+        Assert.assertTrue(driver.getPageSource().contains("Create an Account"));
+
+    }
+
+    @Test
+    public void TC_02() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
